d6886f0e540d598c7a61bc83079b40516a3a239e
Vauquelin Aurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-04-30 08:56:11 +0200

    first commit

---
+++ src/main/java/campus/valence/demineur/BombCase.java
@@ -0,0 +1,14 @@
+package campus.valence.demineur;
+
+public class BombCase {
+    static class BombCase extends Case {
+        public BombCase(int x, int y) {
+            super(x, y);
+        }
+
+        @Override
+        public String toString() {
+            return "BOMB(" + x + ", " + y + ")";
+        }
+    }
+}
